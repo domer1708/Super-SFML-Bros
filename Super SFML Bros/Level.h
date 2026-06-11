a078870433a249e8f87ab2c74001b9a6ff697c4a
Marcello527 <marcel.marciniak@student.put.poznan.pl>
2026-06-11 22:29:26 +0200

    Update Level.h

--- Super SFML Bros/Level.h
+++ Super SFML Bros/Level.h
@@ -36,7 +36,7 @@ public:
     // --- ZAKTUALIZOWANA DEKLARACJA ---
     // --- ZAKTUALIZOWANA DEKLARACJA ---
     bool loadFromFile(const std::string& filename, const sf::Texture& mushroomTex, const sf::Texture& groundTex, const sf::Texture& doorTex, const sf::Texture& trapTex);
-
+    bool loadFromFile(const std::string& filename, const sf::Texture& mushroomTex, const sf::Texture& platformTex);
     const std::vector<sf::RectangleShape>& getPlatforms() const;
     sf::Vector2f getPlayerSpawn() const;
     void render(sf::RenderWindow& window);
